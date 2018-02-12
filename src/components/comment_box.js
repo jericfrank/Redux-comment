43cf0ba3159a5b633cf247cb45b2bb08493ef600
Jeric Frank Sasil <jericfrank@gmail.com>
2018-02-12 17:14:42 +0800

    feat(reducers): add comment reducers

--- src/components/comment_box.js
+++ src/components/comment_box.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import * as actions from '../actions';
 
-export default class CommentBox extends Component {
+class CommentBox extends Component {
 	constructor(props) {
 		super(props);
 
@@ -15,10 +17,9 @@ export default class CommentBox extends Component {
 
 	handleSubmit(event) {
 		event.preventDefault();
-
-		this.setState({
-			comment: ''
-		});
+		
+		this.props.saveComment( this.state.comment );
+		this.setState({ comment: '' });
 	}
 
   render() {
@@ -30,3 +31,5 @@ export default class CommentBox extends Component {
     );
   }
 }
+
+export default connect(null, actions)(CommentBox);
